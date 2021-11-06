83d26547068262627e2916423f1ceb2095918d00
Arecio Canton <cuba289@gmail.com>
2021-11-06 03:58:14 -0400

    We got videogit add .

--- capture.py
+++ capture.py
@@ -1,24 +1,30 @@
-import cv2, time 
+import cv2
 # video obj with method for capturing video (0,1,2 = webcam or path to video)
 # Trigger camera
 video=cv2.VideoCapture(0)
 
-# Frame obj for showing window which reads video obj
-# check = bool / frame = 3D numpy array which rep img (first frame vid captures)
-# Reads 1st Frame
-check, frame = video.read()
+# While Loop through Frame array and use imshow to show each frame in the array
+while True:
+    # Frame obj for showing window which reads video obj
+    # check = bool / frame = 3D numpy array which rep img (first frame vid captures)
+    # Reads 1st Frame
+    check, frame = video.read()
 
-print(check)
-print(frame)
+    print(check)
+    print(frame)
 
- # Loop through Frame array and use imshow to show each frame in the array
 
-# this makes python wait for 3 seconds after turning on camera
-time.sleep(3)
-# Created window and showed 1st frame of video
-cv2.imshow('Capturing',frame)
-# Allows you to press any button and close window
-cv2.waitKey(0)
+    # Converts frame to Grayscale
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+    # Created window and showed 1st frame of video
+    cv2.imshow("Capturing",gray)
+    # Allows you to press any button and close window
+    key=cv2.waitKey(1000)
+    # if you press Q key it stops the loop
+    if key == ord('q'):
+        break
+    
 # release camera
 video.release()
 # Closes window
